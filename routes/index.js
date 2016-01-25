6001a5bc862adfd53a9dec658aaee3dac2e17d10
Alec Rietman <rietmale@oregonstate.edu>
2016-01-25 20:17:33 +0000

    Fixes database name

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'STTserver',
     password: 'tech31',
-    database: 'sttdb'
+    database: 'STTDB'
 });
 
 /* GET home page. */
