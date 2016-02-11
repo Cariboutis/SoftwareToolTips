987ab58d75f78a7f949c9ee036a999030a52e77c
Matt Kottre <mkottre@10-251-1-232.wireless.oregonstate.edu>
2016-02-11 15:44:22 -0800

    edited userprofile.js route

--- routes/userprofile.js
+++ routes/userprofile.js
@@ -1,17 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('userprofile', { title: 'User Profile' });
-});
-
-router.get('/SignUp', function(req,res,next) {
-   res.render('login', {title: 'Log In'});
-});
-
-router.get('/About', function(req,res,next) {
-   res.render('about');
 });
 
 module.exports = router;
